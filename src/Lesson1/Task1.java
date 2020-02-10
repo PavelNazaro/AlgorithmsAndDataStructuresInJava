96df7c555a1da61354fe2a65fed81ac5cf90597d
pavelnazaro <pavelnazaro@mail.ru>
2020-02-10 16:19:02 +0300

    edited

--- src/Lesson1/Task1.java
+++ src/Lesson1/Task1.java
@@ -3,33 +3,20 @@ package Lesson1;
 public class Task1 {
     private static final int arr[] = {-55, 11, -23, -63, 54, 95, 106, 12, -40, 49, 0, 13, -9, 9};
 
-    public static void main(String[] args) {
-        int check = arr.length - 2;
-        if (check > 0) {
-            int a = 0;
-            int b = 1;
-            int c = 2;
-            int i = 0;
-            while (a != check){
-                if (arr[a] + arr[b] + arr[c] == 0) {
-                    System.out.println(arr[a] + " + " + arr[b] + " + " + arr[c] + " = 0");
-                }
-                if (c == arr.length - 1){
-                    if (b == arr.length - 2){
-                        a++;
-                        b = a + 1;
-                    } else {
-                        b++;
+    private static void findThreeSumInArray(int[] arr) {
+        int len = arr.length;
+        for (int i = 0; i < len; i++) {
+            for (int j = i + 1; j < len; j++) {
+                for (int k = j + 1; k < len; k++) {
+                    if (arr[i] + arr[j] + arr[k] == 0) {
+                        System.out.printf("arr[%d]+arr[%d]+arr[%d] == 0%n", i, j, k);
                     }
-                    c = b + 1;
-                } else {
-                    c++;
                 }
-                i++;
             }
-            System.out.println("The number of combinations without repetition: " + i);
-        } else {
-            System.out.println("The array is small!");
         }
+    }
+
+    public static void main(String[] args) {
+        findThreeSumInArray(arr);
     }
 }
--- src/Lesson1/Task2.java
+++ src/Lesson1/Task2.java
@@ -3,7 +3,7 @@ package Lesson1;
 public class Task2 {
     private static final int arr[] = {-55, 11, -23, -63, 54, 95, 106, 12, -40, 49, 0, 13, -9, 9};
 
-    public static void main(String[] args) {
+    private static void findTwoMaxInArray(int[] arr) {
         int max1 = arr[0];
         int max2 = arr[0];
 
@@ -20,5 +20,9 @@ public class Task2 {
 
         System.out.println("Max1: " + max1);
         System.out.println("Max2: " + max2);
+    }
+
+    public static void main(String[] args) {
+        findTwoMaxInArray(arr);
     }
 }
