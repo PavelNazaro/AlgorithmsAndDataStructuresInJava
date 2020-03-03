c476168962be871365cfd935544a033e10d62451
pavelnazaro <pavelnazaro@mail.ru>
2020-03-03 09:47:09 +0300

    Lesson8: add files

---
+++ src/Lesson8/Edge.java
@@ -0,0 +1,20 @@
+package Lesson8;
+
+public class Edge {
+
+    private Vertex vertex;
+    private int weight;
+
+    public Edge(Vertex vertex, int weight) {
+        this.vertex = vertex;
+        this.weight = weight;
+    }
+
+    public Vertex getVertex() {
+        return vertex;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+}
